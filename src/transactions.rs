de1c7065817c271c741c26bfe280046b337b4e53
MoKhalifa9 <96131233+MoKhalifa9@users.noreply.github.com>
2025-08-24 15:30:11 +0300

    added purchase logic (#4)

--- src/transactions.rs
+++ src/transactions.rs
@@ -1,17 +1,28 @@
-struct Sales {
+pub struct Sales {
     product_name: String,
     quantity: u32,
     unit_price: f64,
     total_price: f64,
 }
 
+pub struct Purchase {
+    product_name: String,
+    quantity: u32,
+    unit_price: f64,
+    total_cost: f64,
+}
+
 pub struct Transactions {
     sales: Vec<Sales>,
+    purchases: Vec<Purchase>,
 }
 
 impl Transactions {
     pub fn new() -> Self {
-        Transactions { sales: Vec::new() }
+        Self {
+            sales: Vec::new(),
+            purchases: Vec::new(),
+        }
     }
 
     pub fn record_sale(&mut self, product_name: String, quantity: u32, unit_price: f64) {
@@ -25,7 +36,23 @@ impl Transactions {
         self.sales.push(sale);
     }
 
+    pub fn record_purchase(&mut self, product_name: String, quantity: u32, unit_price: f64) -> &Purchase {
+        let total_cost = quantity as f64 * unit_price;
+        self.purchases.push(Purchase {
+            product_name: product_name.to_string(),
+            quantity,
+            unit_price,
+            total_cost,
+        });
+        self.purchases.last().unwrap()
+    }
+
+    
     pub fn get_sales(&self) -> &Vec<Sales> {
         &self.sales
+    }
+    
+    pub fn get_purchases(&self) -> &[Purchase] {
+        &self.purchases
     }
 }
